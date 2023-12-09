85ca29d0257169c6393c8a56795f12b36bd731e7
mabdou <abdoumelouk96@gmail.com>
2023-12-09 18:45:29 +0100

    Impemented search in search box Fixed CSS for header and recipe cards Made the page responsive until 1024px

--- scripts/generate.js
+++ scripts/generate.js
@@ -1,8 +1,9 @@
 import recipes from '../data/recipes.mjs'
-import { getAllIngredients, getAllAppliances, getAllUstensils } from './methods.js'
+import { getAllIngredients, getAllAppliances, getAllUstensils, searchRecipesByTitleIngredientsDescription } from './methods.js'
 
-function generateRecipeCard() {
+function generateRecipeCard(recipes) {
   const recipesContainer = document.querySelector('.recipes-container')
+  recipesContainer.innerHTML = ''
   recipes.map(recipe => {
     const recipeCard = document.createElement('article')
     recipeCard.classList.add('recipe-card')
@@ -16,33 +17,33 @@ function generateRecipeCard() {
       </section>
       <section class='ingredients-container'>
         <h5>Ingrédients</h5>
-        <ul class='ingredients'>  
-          ${recipe.ingredients
-            .map(
-              ingredient => `
-            <li class='ingredient'>
-              <p class='ingredient-name'>${ingredient.ingredient}</p>
-              ${
-                ingredient.quantity
-                  ? `<p class='ingredient-quantity'>${ingredient.quantity} 
-                  ${ingredient.unit ? ` ${ingredient.unit}` : ''}
-                </p>`
-                  : ''
-              }
-            </li>
-          `
-            )
-            .join('')}
+        <ul class='ingredients'>
+        ${recipe.ingredients
+          .map(ingredient => {
+            //Check if ingredient quantity has a unit
+            let unit = ingredient.unit ? `${ingredient.unit}` : ''
+            //Check if ingredient has a quantity
+            let quantity = ingredient.quantity ? `<p class='ingredient-quantity'>${ingredient.quantity} ${unit}</p>` : ''
+            return `<li class='ingredient'><p class='ingredient-name'>${ingredient.ingredient} ${quantity}</p> </li>`
+          })
+          .join('')}
         </ul>
-      </section>
-    `
+      </section>`
     recipesContainer.appendChild(recipeCard)
   })
 }
 
-function generateIngredients() {
+function updateTotalRecipes(recipes) {
+  const totalRecipes = document.querySelector('.total')
+  if (recipes.length > 1) totalRecipes.innerHTML = `${recipes.length} recettes`
+  else if (recipes.length === 1) totalRecipes.innerHTML = `${recipes.length} recette`
+  else if (recipes.length === 0) totalRecipes.innerHTML = `Aucune recette trouvée`
+}
+
+function generateIngredients(recipes) {
   const allIngredients = getAllIngredients(recipes)
   const ingredientsMenu = document.querySelector('.menu.ingredients .menu__items')
+  ingredientsMenu.innerHTML = ''
   allIngredients.map(ingredient => {
     const ingredientItem = document.createElement('li')
     ingredientItem.classList.add('menu__item')
@@ -53,9 +54,10 @@ function generateIngredients() {
   })
 }
 
-function generateAppliances() {
+function generateAppliances(recipes) {
   const allAppliances = getAllAppliances(recipes)
   const appliancesMenu = document.querySelector('.menu.appareils .menu__items')
+  appliancesMenu.innerHTML = ''
   allAppliances.map(appliance => {
     const applianceItem = document.createElement('li')
     applianceItem.classList.add('menu__item')
@@ -66,9 +68,10 @@ function generateAppliances() {
   })
 }
 
-function generateUstensils() {
+function generateUstensils(recipes) {
   const allUstensils = getAllUstensils(recipes)
   const ustensilsMenu = document.querySelector('.menu.ustensils .menu__items')
+  ustensilsMenu.innerHTML = ''
   allUstensils.map(ustensil => {
     const ustensilItem = document.createElement('li')
     ustensilItem.classList.add('menu__item')
@@ -79,7 +82,26 @@ function generateUstensils() {
   })
 }
 
-generateIngredients()
-generateAppliances()
-generateUstensils()
-generateRecipeCard()
+generateIngredients(recipes)
+generateAppliances(recipes)
+generateUstensils(recipes)
+generateRecipeCard(recipes)
+updateTotalRecipes(recipes)
+
+function searchBox() {
+  const searchButton = document.querySelector('.search-button')
+  searchButton.addEventListener('click', () => {
+    const searchInput = document.getElementById('search-value')
+    const value = searchInput.value
+    if (searchInput.value.length >= 3) {
+      const filteredRecipes = searchRecipesByTitleIngredientsDescription(recipes, value)
+      generateRecipeCard(filteredRecipes)
+      generateIngredients(filteredRecipes)
+      generateAppliances(filteredRecipes)
+      generateUstensils(filteredRecipes)
+      updateTotalRecipes(filteredRecipes)
+    }
+  })
+}
+
+searchBox()
--- scripts/methods.js
+++ scripts/methods.js
@@ -1,71 +1,9 @@
-export function getRecipesByIngredient(recipes, ingredient) {
-  const result = []
-  recipes.map(recipe => {
-    recipe.ingredients.map(ingredients => {
-      if (ingredients.ingredient.toLowerCase() === ingredient.toLowerCase()) {
-        result.push(recipe)
-      }
-    })
-  })
-  return result
-}
-
-export function getRecipesByUstensil(recipes, ustensil) {
+//3 functions to generate lists of ingredients, appliances and ustensils
+export function getAllIngredients(recipes) {
   let result = []
   recipes.map(recipe => {
-    recipe.ustensils.map(ustensils => {
-      if (ustensils.toLowerCase() === ustensil.toLowerCase()) {
-        result.push(recipe)
-      }
-    })
-  })
-}
-
-export function getRecipesByAppliance(recipes, appliance) {
-  const result = []
-  recipes.map(recipe => {
-    if (recipe.appliance.toLowerCase() === appliance.toLowerCase()) {
-      result.push(recipe)
-    }
-  })
-  return result
-}
-
-export function getRecipesByTitle(recipes, title) {
-  const result = []
-  recipes.map(recipe => {
-    if (recipe.name.toLowerCase().includes(title.toLowerCase())) {
-      result.push(recipe)
-    }
-  })
-  return result
-}
-
-export function getRecipesByDescription(recipes, description) {
-  const result = []
-  recipes.map(recipe => {
-    if (recipe.description.toLowerCase().includes(description.toLowerCase())) {
-      result.push(recipe)
-    }
-  })
-  return result
-}
-
-export function getRecipesByTitleIngredientsDescription(recipes, search) {
-  const tempTitle = getRecipesByTitle(recipes, search)
-  const tempIngredients = getRecipesByIngredient(recipes, search)
-  const tempDescription = getRecipesByDescription(recipes, search)
-
-  let temp = [...tempTitle, ...tempIngredients, ...tempDescription]
-  const result = [...new Set(temp)]
-
-  return result
-}
-
-export function getAllIngredients(recipes) {
-  const result = []
-  recipes.map(recipe => {
     recipe.ingredients.map(ingredient => {
+      ingredient.ingredient = ingredient.ingredient.toLowerCase()
       result.push(ingredient.ingredient)
     })
   })
@@ -88,4 +26,84 @@ export function getAllUstensils(recipes) {
     })
   })
   return [...new Set(result)]
+}
+
+//3 functions to filter recipes by ingredients, appliances and ustensils
+export function getRecipesByIngredient(recipes, ingredient) {
+  const result = []
+  recipes.map(recipe => {
+    recipe.ingredients.map(ingredients => {
+      if (ingredients.ingredient.toLowerCase() === ingredient.toLowerCase()) {
+        result.push(recipe)
+      }
+    })
+  })
+  return result
+}
+
+export function getRecipesByAppliance(recipes, appliance) {
+  const result = []
+  recipes.map(recipe => {
+    if (recipe.appliance.toLowerCase() === appliance.toLowerCase()) {
+      result.push(recipe)
+    }
+  })
+  return result
+}
+
+export function getRecipesByUstensil(recipes, ustensil) {
+  let result = []
+  recipes.map(recipe => {
+    recipe.ustensils.map(ustensils => {
+      if (ustensils.toLowerCase() === ustensil.toLowerCase()) {
+        result.push(recipe)
+      }
+    })
+  })
+  return result
+}
+
+//3 functions to search recipes by title, ingredient and description
+export function searchRecipesByTitle(recipes, searchValue) {
+  const result = []
+  recipes.map(recipe => {
+    if (containsWord(recipe.name, searchValue)) result.push(recipe)
+  })
+  return result
+}
+
+export function searchRecipesByIngredients(recipes, searchValue) {
+  const result = []
+  recipes.map(recipe => {
+    recipe.ingredients.map(ingredient => {
+      if (containsWord(ingredient.ingredient, searchValue)) result.push(recipe)
+    })
+  })
+  return result
+}
+
+export function searchRecipesByDescription(recipes, searchvalue) {
+  const result = []
+  recipes.map(recipe => {
+    if (containsWord(recipe.description, searchvalue)) result.push(recipe)
+  })
+  return result
+}
+
+export function searchRecipesByTitleIngredientsDescription(recipes, search) {
+  const tempTitle = searchRecipesByTitle(recipes, search)
+  const tempIngredients = searchRecipesByIngredients(recipes, search)
+  const tempDescription = searchRecipesByDescription(recipes, search)
+
+  let temp = [...tempTitle, ...tempIngredients, ...tempDescription]
+  const result = [...new Set(temp)]
+
+  return result
+}
+
+export function containsWord(originalString, wordToTest) {
+  wordToTest = wordToTest.toLowerCase()
+  originalString = originalString.toLowerCase()
+  const regex = new RegExp(`\\b${wordToTest}\\b`, 'g')
+  return regex.test(originalString)
 }
