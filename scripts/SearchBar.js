bfa49caaf458d44f7a2b992468e630eef8c88abf
mabdou <abdoumelouk96@gmail.com>
2023-12-11 21:50:14 +0100

    Filter recipes on keyup instead of click Search bar test by string, not word Refactor UI updates to single function call

--- scripts/SearchBar.js
+++ scripts/SearchBar.js
@@ -9,7 +9,7 @@ import recipes from '../data/recipes.mjs'
 function searchRecipesByTitle(recipes, searchValue) {
   let result = []
   recipes.map(recipe => {
-    if (containsWord(recipe.name, searchValue)) result.push(recipe)
+    if (recipe.name.toLowerCase().includes(searchValue.toLowerCase())) result.push(recipe)
   })
   return [...new Set(result)]
 }
@@ -18,7 +18,7 @@ function searchRecipesByIngredients(recipes, searchValue) {
   let result = []
   recipes.map(recipe => {
     recipe.ingredients.map(ingredient => {
-      if (containsWord(ingredient.ingredient, searchValue)) result.push(recipe)
+      if (ingredient.ingredient.toLowerCase().includes(searchValue.toLowerCase())) result.push(recipe)
     })
   })
   return [...new Set(result)]
@@ -27,7 +27,7 @@ function searchRecipesByIngredients(recipes, searchValue) {
 function searchRecipesByDescription(recipes, searchvalue) {
   let result = []
   recipes.map(recipe => {
-    if (containsWord(recipe.description, searchvalue)) result.push(recipe)
+    if (recipe.description.toLowerCase().includes(searchvalue.toLowerCase())) result.push(recipe)
   })
   return [...new Set(result)]
 }
@@ -43,20 +43,32 @@ function searchRecipesByTitleIngredientsDescription(recipes, search) {
 
 function searchBox() {
   let filteredRecipes = []
-  const searchButton = document.querySelector('.search-button')
-  searchButton.addEventListener('click', () => {
-    const searchInput = document.getElementById('search-value')
-    const value = searchInput.value
+  const searchInput = document.getElementById('search-value')
+  searchInput.addEventListener('keyup', () => {
     if (searchInput.value.length >= 3) {
+      const value = searchInput.value
       filteredRecipes = searchRecipesByTitleIngredientsDescription(recipes, value)
-      generateRecipeCard(filteredRecipes)
-      generateIngredients(filteredRecipes)
-      generateAppliances(filteredRecipes)
-      generateUstensils(filteredRecipes)
-      updateTotalRecipes(filteredRecipes)
-      sessionStorage.setItem('filteredRecipes', JSON.stringify(filteredRecipes))
+      updatePage(filteredRecipes, value)
     }
   })
+
   return filteredRecipes
 }
+function resetInputs() {
+  const inputs = document.querySelectorAll('.filter.search-bar input')
+  inputs.forEach(input => {
+    input.value = ''
+  })
+}
+
+function updatePage(filteredRecipes) {
+  generateRecipeCard(filteredRecipes)
+  generateIngredients(filteredRecipes)
+  generateAppliances(filteredRecipes)
+  generateUstensils(filteredRecipes)
+  updateTotalRecipes(filteredRecipes)
+  resetInputs()
+  sessionStorage.setItem('filteredRecipes', JSON.stringify(filteredRecipes))
+}
+
 searchBox()
