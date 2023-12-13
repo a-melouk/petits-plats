a45dde7f9103c1a9a2767aea0d1e2700908eee57
mabdou <abdoumelouk96@gmail.com>
2023-12-13 20:46:43 +0100

    Added a delay of 200ms before launching the search algorithm

--- scripts/SearchBar.js
+++ scripts/SearchBar.js
@@ -8,19 +8,23 @@ import { searchRecipesByTitleIngredientsDescription } from './Search.js'
 
 function searchBox() {
   let filteredRecipes = []
+  let timeout = null
   const searchInput = document.getElementById('search-value')
   searchInput.addEventListener('keyup', () => {
-    if (searchInput.value.length >= 3) {
-      const value = searchInput.value
-      filteredRecipes = searchRecipesByTitleIngredientsDescription(recipes, value)
-      updatePage(filteredRecipes, value)
-      saveHistory(filteredRecipes)
-      // menuIngredients()
-    } else if (searchInput.value.length < 3) {
-      filteredRecipes = recipes
-      updatePage(filteredRecipes)
-      saveHistory(filteredRecipes)
-    }
+    clearTimeout(timeout)
+    timeout = setTimeout(function () {
+      if (searchInput.value.length >= 3) {
+        const value = searchInput.value
+        filteredRecipes = searchRecipesByTitleIngredientsDescription(recipes, value)
+        updatePage(filteredRecipes, value)
+        saveHistory(filteredRecipes)
+        // menuIngredients()
+      } else if (searchInput.value.length < 3) {
+        filteredRecipes = recipes
+        updatePage(filteredRecipes)
+        saveHistory(filteredRecipes)
+      }
+    }, 200)
   })
 
   return filteredRecipes
